2b72f056c543ed385bb409c397729a2666ba09ea
Daniel Samek <samekdaniel@email.cz>
2025-04-04 00:21:40 +0200

    take 4

--- src/search.cpp
+++ src/search.cpp
@@ -1454,7 +1454,7 @@ moves_loop:  // When in check, search starts here
            + 141 * (!ss->inCheck && bestValue <= ss->staticEval - 100)
            + 121 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 75)
            + 86  * ((ss - 1)->isTTMove) + 86 * (ss->cutoffCnt <= 3)
-           + 33  * ((ss + 1)->pvCutoffCnt <= 1)
+           + 48  * ((ss + 1)->pvCutoffCnt <= 1 && PvNode)
            + std::min(-(ss - 1)->statScore / 112, 303));
 
         bonusScale = std::max(bonusScale, 0);
