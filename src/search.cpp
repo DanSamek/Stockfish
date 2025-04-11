5edca48589d60e0ff6c3e1474333c5efe9de98c4
Daniel Samek <samekdaniel@email.cz>
2025-04-11 13:53:51 +0200

    take 1
    bench 1913779

--- src/search.cpp
+++ src/search.cpp
@@ -168,7 +168,8 @@ void update_all_stats(const Position&      pos,
                       ValueList<Move, 32>& capturesSearched,
                       Depth                depth,
                       bool                 isTTMove,
-                      int                  moveCount);
+                      int                  moveCount,
+                      bool                 failHigh);
 
 }  // namespace
 
@@ -1435,7 +1436,7 @@ moves_loop:  // When in check, search starts here
     else if (bestMove)
     {
         update_all_stats(pos, ss, *this, bestMove, prevSq, quietsSearched, capturesSearched, depth,
-                         bestMove == ttData.move, moveCount);
+                         bestMove == ttData.move, moveCount, bestValue >= beta);
         if (!PvNode)
         {
             int bonus = (ttData.move == move) ? 800 : -600 * moveCount;
@@ -1847,13 +1848,14 @@ void update_all_stats(const Position&      pos,
                       ValueList<Move, 32>& capturesSearched,
                       Depth                depth,
                       bool                 isTTMove,
-                      int                  moveCount) {
+                      int                  moveCount,
+                      bool                 failHigh) {
 
     CapturePieceToHistory& captureHistory = workerThread.captureHistory;
     Piece                  moved_piece    = pos.moved_piece(bestMove);
     PieceType              captured;
 
-    int bonus = std::min(141 * depth - 89, 1613) + 311 * isTTMove;
+    int bonus = std::min(141 * depth - 89, 1613) + 311 * isTTMove + std::min(5 * depth, 50) * failHigh;
     int malus = std::min(695 * depth - 215, 2808) - 31 * (moveCount - 1);
 
     if (!pos.capture_stage(bestMove))
