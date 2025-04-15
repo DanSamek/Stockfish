b92a571e368312cfcf112394e36ef5b0bc1c454a
Daniel Samek <samekdaniel@email.cz>
2025-04-15 15:55:14 +0200

    take 2
    bench 1818493

--- src/search.cpp
+++ src/search.cpp
@@ -1070,7 +1070,7 @@ moves_loop:  // When in check, search starts here
 
                 // SEE based pruning for captures and checks
                 int seeHist = std::clamp(captHist / 32, -138 * depth, 135 * depth);
-                int eb      = (eval - beta) / 16;
+                int eb      = (eval - beta) / 8;
                 if (!pos.see_ge(move, -154 * depth - seeHist - eb))
                     continue;
             }
