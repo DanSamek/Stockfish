a5f8472e58944ac3eae262d8a096aa94636778d2
Daniel Samek <samekdaniel@email.cz>
2025-04-20 22:08:17 +0200

    take 1
    bench 1804933

--- src/search.cpp
+++ src/search.cpp
@@ -1462,6 +1462,7 @@ moves_loop:  // When in check, search starts here
            + 141 * (!ss->inCheck && bestValue <= ss->staticEval - 100)
            + 121 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 75)
            + 86 * ((ss - 1)->isTTMove) + 86 * (ss->cutoffCnt <= 3)
+           + 64 * (priorReduction >= 3)
            + std::min(-(ss - 1)->statScore / 112, 303));
 
         bonusScale = std::max(bonusScale, 0);
