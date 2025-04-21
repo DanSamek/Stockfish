a97a4cee538bc2d7b7683e2fd8f2a7acd2c9b7c6
Daniel Samek <samekdaniel@email.cz>
2025-04-21 16:44:16 +0200

    take 1
    bench 1671601

--- src/search.cpp
+++ src/search.cpp
@@ -844,7 +844,7 @@ Value Search::Worker::search(
 
     if (priorReduction >= 3 && !opponentWorsening)
         depth++;
-    if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188)
+    if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 188 && is_valid((ss - 1)->staticEval))
         depth--;
 
     // Step 7. Razoring
