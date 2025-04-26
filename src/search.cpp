b0c876ab9f1d4e5463c46a0208d19f59b9d097bf
Daniel Samek <samekdaniel@email.cz>
2025-04-26 19:57:23 +0200

    take 4
    bench 1610376

--- src/search.cpp
+++ src/search.cpp
@@ -919,7 +919,7 @@ Value Search::Worker::search(
     // Step 11. ProbCut
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
-    probCutBeta = beta + 185 - 58 * improving + 29 * !longTermImproving;
+    probCutBeta = beta + 185 - 58 * longTermImproving;
     if (depth >= 3
         && !is_decisive(beta)
         // If value from transposition table is lower than probCutBeta, don't attempt
