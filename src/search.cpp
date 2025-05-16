c8ba0ffb80e27195532ab9e8657d71e969136d5a
Daniel Samek <samekdaniel@email.cz>
2025-05-16 14:27:21 +0200

    take 1
    bench 2251727

--- src/search.cpp
+++ src/search.cpp
@@ -605,7 +605,7 @@ Value Search::Worker::search(
     Value bestValue, value, eval, maxValue, probCutBeta;
     bool  givesCheck, improving, priorCapture, opponentWorsening;
     bool  capture, ttCapture;
-    int   priorReduction;
+    int   priorReduction, razoringDepth;
     Piece movedPiece;
 
     ValueList<Move, 32> capturesSearched;
@@ -815,10 +815,12 @@ Value Search::Worker::search(
     if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 175)
         depth--;
 
+    razoringDepth = depth + (improving && (ss - 1)->isPvNode);
+
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 486 - 325 * depth * depth)
+    if (!PvNode && eval < alpha - 486 - 325 * razoringDepth * razoringDepth)
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node
