a19185270b9255c6622a2a8508ff7badd5a71d62
Daniel Samek <samekdaniel@email.cz>
2025-05-17 23:22:42 +0200

    try
    bench 2316113

--- src/search.cpp
+++ src/search.cpp
@@ -815,12 +815,6 @@ Value Search::Worker::search(
     if (priorReduction >= 1 && depth >= 2 && ss->staticEval + (ss - 1)->staticEval > 175)
         depth--;
 
-    // Step 7. Razoring
-    // If eval is really low, skip search entirely and return the qsearch value.
-    // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 486 - 325 * depth * depth)
-        return qsearch<NonPV>(pos, ss, alpha, beta);
-
     // Step 8. Futility pruning: child node
     // The depth condition is important for mate finding.
     {
