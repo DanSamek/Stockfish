fad072b13894ac28a2c307e2df9b317b01b77bd5
Daniel Samek <samekdaniel@email.cz>
2025-05-19 15:12:01 +0200

    take 1
    bench 2012032

--- src/search.cpp
+++ src/search.cpp
@@ -1413,6 +1413,9 @@ moves_loop:  // When in check, search starts here
             }
         }
 
+        if (value < alpha - 500 && extension > 1 && depth >= 10)
+            depth--;
+
         // If the move is worse than some previously searched move,
         // remember it, to update its stats later.
         if (move != bestMove && moveCount <= 32)
