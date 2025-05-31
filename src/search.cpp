16a2d5d6f5e25779736af020f8c40082d1781c2f
Daniel Samek <samekdaniel@email.cz>
2025-05-31 19:01:45 +0200

    take 1
    bench 2281372

--- src/search.cpp
+++ src/search.cpp
@@ -1464,6 +1464,9 @@ moves_loop:  // When in check, search starts here
         if (type_of(pos.piece_on(prevSq)) != PAWN && ((ss - 1)->currentMove).type_of() != PROMOTION)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
               << scaledBonus * 1040 / 32768;
+
+        if ((ss - 1)->ply < LOW_PLY_HISTORY_SIZE)
+            thisThread->lowPlyHistory[(ss - 1)->ply][(ss - 1)->currentMove.from_to()] << scaledBonus * 250 / 32768;
     }
 
     // Bonus for prior capture countermove that caused the fail low
