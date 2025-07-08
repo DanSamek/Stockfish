5e7626e4c9da5401c87b30729d6ccd5b810ac2cf
Daniel Samek <samekdaniel@email.cz>
2025-07-08 19:51:51 +0200

    60k games tune
    bench 2115335

--- src/search.cpp
+++ src/search.cpp
@@ -1890,7 +1890,7 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
     static constexpr std::array<ConthistBonus, 6> conthist_bonuses = {
       {{1, 1092}, {2, 631}, {3, 294}, {4, 517}, {5, 126}, {6, 445}}};
 
-    static constexpr int chp[6] {66, 99, 5, -34, 40, -45};
+    static constexpr int chp[6] {71, 106, -22, -20, 29, -74};
 
     for (const auto [i, weight] : conthist_bonuses)
     {
@@ -1911,14 +1911,14 @@ void update_quiet_histories(
     workerThread.mainHistory[us][move.from_to()] << bonus;  // Untuned to prevent duplicate effort
 
     if (ss->ply < LOW_PLY_HISTORY_SIZE)
-        workerThread.lowPlyHistory[ss->ply][move.from_to()] << (bonus * 792 / 1024) + 39;
+        workerThread.lowPlyHistory[ss->ply][move.from_to()] << (bonus * 792 / 1024) + 40;
 
     update_continuation_histories(ss, pos.moved_piece(move), move.to_sq(),
                                   bonus * (bonus > 0 ? 1082 : 784) / 1024);
 
     int pIndex = pawn_structure_index(pos);
     workerThread.pawnHistory[pIndex][pos.moved_piece(move)][move.to_sq()]
-      << (bonus * (bonus > 0 ? 705 : 450) / 1024) + 65;
+      << (bonus * (bonus > 0 ? 705 : 450) / 1024) + 70;
 }
 
 }
