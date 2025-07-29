c8f8e03014eb03b1844ce0ea062b2b842f6161b4
Daniel Samek <samekdaniel@email.cz>
2025-07-29 21:47:00 +0200

    Fix UB...
    bench 3420537

--- src/search.cpp
+++ src/search.cpp
@@ -61,10 +61,10 @@ void syzygy_extend_pv(const OptionsMap&            options,
 
 using namespace Search;
 
-int a1, a2, a3, a4, a5, a6, a7, a8;
-int lph[LOW_PLY_HISTORY_SIZE];
-int cph[6];
-TUNE(SetRange(-1024,1024), a1, a2, a3, a4, a5, a6, a7, a8, lph, cph);
+int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a6 = 0, a7 = 0, a8 = 0;
+int lph[LOW_PLY_HISTORY_SIZE] = {0, 0, 0, 0, 0};
+int cph[6] = {0, 0, 0, 0, 0, 0};
+TUNE(SetRange(-1024,1024), a1, a2, a3, a4, a6, a7, a8, lph, cph);
 
 namespace {
 
