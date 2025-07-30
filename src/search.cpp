e797f230c290834a0be1c2b16a9c94eee808c2f8
Daniel Samek <samekdaniel@email.cz>
2025-07-30 20:16:00 +0200

    try
    bench 3121063

--- src/search.cpp
+++ src/search.cpp
@@ -1353,7 +1353,7 @@ moves_loop:  // When in check, search starts here
                 if (value >= beta)
                 {
                     // (* Scaler) Especially if they make cutoffCnt increment more often.
-                    ss->cutoffCnt += (extension < 2) || PvNode;
+                    ss->cutoffCnt += (extension < 2 && cutNode) || PvNode;
                     assert(value >= beta);  // Fail high
                     break;
                 }
