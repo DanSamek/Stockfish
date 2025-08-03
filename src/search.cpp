f8490a5bcb71112eca11200f0f29ecfa352c8acb
Daniel Samek <samekdaniel@email.cz>
2025-08-03 10:48:09 +0200

    Simplify cutoffCnt reduction
    bench 3002319

--- src/search.cpp
+++ src/search.cpp
@@ -1193,7 +1193,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 2)
-            r += 935 + allNode * 763;
+            r += 1150;
 
         r += (ss + 1)->quietMoveStreak * 51;
 
