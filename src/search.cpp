fcfa7b3d8cc93cb9875e6406e2a36699ce7d2404
Daniel Samek <samekdaniel@email.cz>
2025-08-15 18:35:31 +0200

    take 1 - 50k games STC tune
    bench 3508198

--- src/search.cpp
+++ src/search.cpp
@@ -61,9 +61,6 @@ void syzygy_extend_pv(const OptionsMap&            options,
 
 using namespace Search;
 
-int a1 = -753, a2 = 1042, a3 = 1288, a4 = 1431;
-TUNE(a1, a2, a3, a4);
-
 namespace {
 
 constexpr int SEARCHEDLIST_CAPACITY = 32;
@@ -548,7 +545,7 @@ void Search::Worker::undo_null_move(Position& pos) { pos.undo_null_move(); }
 // Reset histories, usually before a new game
 void Search::Worker::clear() {
     mainHistory.fill(64);
-    captureHistory.fill(a1);
+    captureHistory.fill(-755);
     pawnHistory.fill(-1275);
     pawnCorrectionHistory.fill(5);
     minorPieceCorrectionHistory.fill(0);
@@ -1438,7 +1435,7 @@ moves_loop:  // When in check, search starts here
     {
         Piece capturedPiece = pos.captured_piece();
         assert(capturedPiece != NO_PIECE);
-        captureHistory[pos.piece_on(prevSq)][ss->improving][prevSq][type_of(capturedPiece)] << a2;
+        captureHistory[pos.piece_on(prevSq)][ss->improving][prevSq][type_of(capturedPiece)] << 1054;
     }
 
     if (PvNode)
@@ -1838,7 +1835,7 @@ void update_all_stats(const Position& pos,
     {
         // Increase stats for the best move in case it was a capture move
         capturedPiece = type_of(pos.piece_on(bestMove.to_sq()));
-        captureHistory[movedPiece][ss->improving][bestMove.to_sq()][capturedPiece] << captureBonus * a3 / 1024;
+        captureHistory[movedPiece][ss->improving][bestMove.to_sq()][capturedPiece] << captureBonus * 1310 / 1024;
     }
 
     // Extra penalty for a quiet early move that was not a TT move in
@@ -1852,7 +1849,7 @@ void update_all_stats(const Position& pos,
     {
         movedPiece    = pos.moved_piece(move);
         capturedPiece = type_of(pos.piece_on(move.to_sq()));
-        captureHistory[movedPiece][ss->improving][move.to_sq()][capturedPiece] << -captureMalus * a4 / 1024;
+        captureHistory[movedPiece][ss->improving][move.to_sq()][capturedPiece] << -captureMalus * 1460 / 1024;
     }
 }
 
