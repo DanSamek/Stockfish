bef838e7cfb396e74aa1f6991eef84b21cafd345
Daniel Samek <samekdaniel@email.cz>
2025-11-22 10:23:56 +0100

    take 1
    bench 2559177

--- src/search.cpp
+++ src/search.cpp
@@ -1251,6 +1251,9 @@ moves_loop:  // When in check, search starts here
             if (!ttData.move)
                 r += 1140;
 
+            if ((ss + 1)->cutoffCnt > 3)
+                r += 512;
+
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
                                    newDepth - (r > 3957) - (r > 5654 && newDepth > 2), !cutNode);
