a30403ba6f838d1ad4a50add3ede05188c972bd1
Daniel Samek <samekdaniel@email.cz>
2025-12-07 17:24:21 +0100

    take 1
    bench 2440773

--- src/search.cpp
+++ src/search.cpp
@@ -860,6 +860,9 @@ Value Search::Worker::search(
         if (!ttHit && type_of(pos.piece_on(prevSq)) != PAWN
             && ((ss - 1)->currentMove).type_of() != PROMOTION)
             pawnHistory[pawn_history_index(pos)][pos.piece_on(prevSq)][prevSq] << evalDiff * 13;
+
+        if ((ss - 1)->ply < LOW_PLY_HISTORY_SIZE)
+            lowPlyHistory[(ss - 1)->ply][((ss - 1)->currentMove).raw()] << evalDiff * 4;
     }
 
 
