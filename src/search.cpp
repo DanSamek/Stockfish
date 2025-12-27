cf103c20fc10f07c68bdc88a961316cc3dabc731
Daniel Samek <samekdaniel@email.cz>
2025-12-27 22:51:33 +0100

    take 1
    bench 3038865

--- src/search.cpp
+++ src/search.cpp
@@ -1377,6 +1377,9 @@ moves_loop:  // When in check, search starts here
                 assert(depth > 0);
                 alpha = value;  // Update alpha! Always alpha < beta
             }
+            else if (ttData.bound == BOUND_LOWER && extension > 0
+                     && !is_decisive(value) && value + 20 * depth <= beta)
+                depth--;
         }
 
         // If the move is worse than some previously searched move,
