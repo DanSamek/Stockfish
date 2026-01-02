0c4d1a7cb6176a9d97cc04c10871a19c4d06db36
Daniel Samek <samekdaniel@email.cz>
2026-01-02 21:33:02 +0100

    take 2
    bench 2766984

--- src/search.cpp
+++ src/search.cpp
@@ -984,7 +984,7 @@ moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
     probCutBeta = beta + 418;
-    if ((ttData.bound & BOUND_LOWER) && (PvNode ? ss->ttPv && ttData.depth >= depth : ttData.depth >= depth - 4)
+    if ((ttData.bound & BOUND_LOWER) && (PvNode ? ss->ttPv && ttData.depth >= depth - 2 : ttData.depth >= depth - 4)
         && ttData.value >= probCutBeta && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
 
