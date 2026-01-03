4435273a5c200bad6c284c2790ff99af5c3189d8
Daniel Samek <samekdaniel@email.cz>
2026-01-03 20:24:54 +0100

    take 1
    bench 1996907

--- src/search.cpp
+++ src/search.cpp
@@ -1046,11 +1046,14 @@ moves_loop:  // When in check, search starts here
         if (ss->ttPv)
             r += 946;
 
+        if (!ttData.move)
+            r += 256;
+
         // Step 14. Pruning at shallow depths.
         // Depth conditions are important for mate finding.
         if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
         {
-            // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
+            // Skip quiet moves if movecount exceeds our threshold
             if (moveCount >= (3 + depth * depth) / (2 - improving))
                 mp.skip_quiet_moves();
 
