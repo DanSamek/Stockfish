bc61996db85b66ecf07ed66ab15d2eb25b473408
Daniel Samek <samekdaniel@email.cz>
2026-02-22 18:55:55 +0100

    adjust threshold to 3300
    bench 2543845

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -63,7 +63,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     bool smallNet = use_smallnet(pos);
     if (smallNet)
     {
-        smallNet = std::abs(networks.mini.evaluate(miniAccumulators.current())) > 2750;
+        smallNet = std::abs(networks.mini.evaluate(miniAccumulators.current())) > 3300;
     }
 
     auto [psqt, positional] = smallNet ? networks.small.evaluate(pos, accumulators, caches.small)
@@ -81,8 +81,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
                 Hit #0: Total 288166 Hits 32912 Hit Rate (%) 11.4212
                 Hit #1: Total 288166 Hits 255254 Hit Rate (%) 88.5788
             use_smallnet & mininet:
-                Hit #0: Total 246705 Hits 12689 Hit Rate (%) 5.14339
-                Hit #1: Total 246705 Hits 234016 Hit Rate (%) 94.8566
+                Hit #0: Total 211354 Hits 8209 Hit Rate (%) 3.88401
+                Hit #1: Total 211354 Hits 203145 Hit Rate (%) 96.116
     }
     */
 
