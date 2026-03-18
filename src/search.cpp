84863c225e13971e6f846125f2675e43711e00f3
Daniel Samek <samekdaniel@email.cz>
2026-03-18 11:47:02 +0100

    take 2
    bench 2862021

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop:  // When in check, search starts here
             else if (value >= beta && !is_decisive(value))
             {
                 ttMoveHistory << std::max(-394 - 105 * depth, -3692);
-                return (beta * singularDepth + value) / (singularDepth + 1);
+                return (value * singularDepth + beta) / (singularDepth + 1);
             }
 
             // Negative extensions
