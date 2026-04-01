d5d12d25b2178037ae79413da9eb206701aeeb91
Daniel Samek <samekdaniel@email.cz>
2026-04-01 21:04:50 +0200

    try
    bench 3548425

--- src/search.cpp
+++ src/search.cpp
@@ -1301,9 +1301,12 @@ moves_loop:  // When in check, search starts here
 
             // Extend move from transposition table if we are about to dive into qsearch.
             // decisive score handling improves mate finding and retrograde analysis.
+            const bool stillFollowsPV = ss->followPV && static_cast<size_t>(ss->ply) < lastIterationPV.size()
+                                        && ss->currentMove == lastIterationPV[ss->ply];
+
             if ((move == ttData.move
                 && ((is_valid(ttData.value) && is_decisive(ttData.value) && ttData.depth > 0)
-                    || ttData.depth > 1)) || ss->followPV)
+                    || ttData.depth > 1)) || stillFollowsPV)
                 newDepth = std::max(newDepth, 1);
 
             value = -search<PV>(pos, ss + 1, -beta, -alpha, newDepth, false);
