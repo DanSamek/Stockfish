e976d4346eb6cf837211f8473eb776c86f0f5b31
Daniel Samek <samekdaniel@email.cz>
2026-07-06 20:42:18 +0200

    take 3
    bench 2837824

--- src/search.cpp
+++ src/search.cpp
@@ -1227,7 +1227,7 @@ moves_loop:  // When in check, search starts here
 
         // (*Scaler) Generally, higher singularBeta (i.e closer to ttValue)
         // and lower extension margins scale well.
-        if (!rootNode && move == ttData.move && !excludedMove && depth >= 6 + ss->ttPv - (seHistory[se_history_key(pos)][pos.moved_piece(move)][move.to_sq()] / 6144)
+        if (!rootNode && move == ttData.move && !excludedMove && depth >= 6 + ss->ttPv + (seHistory[se_history_key(pos)][pos.moved_piece(move)][move.to_sq()] / 6144)
             && is_valid(ttData.value) && !is_decisive(ttData.value) && (ttData.bound & BOUND_LOWER)
             && ttData.depth >= depth - 3 && !is_shuffling(move, ss, pos))
         {
