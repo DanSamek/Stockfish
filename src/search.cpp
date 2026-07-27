8887b0b1dd1e47db5640ac6b56606606dd8202af
Daniel Samek <samekdaniel@email.cz>
2026-07-27 20:54:49 +0200

    use tteval or static eval for the improving flag
    bench 2441496

--- src/search.cpp
+++ src/search.cpp
@@ -1027,7 +1027,7 @@ Value Search::Worker::search(
         }
     }
 
-    improving |= ss->staticEval >= beta;
+    improving |= eval >= beta;
 
     // Step 10. Internal iterative reductions
     // At sufficient depth, reduce depth for PV/Cut nodes without a TTMove.
